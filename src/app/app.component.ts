bac9e2f129c544d3b732ab8e55e2d00540c5827c
Florian Borie <florian.borie@ynov.com>
2021-06-18 14:17:10 +0200

    begin of traning

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
+  
+@Component({
+  selector: 'pokemon-app',
+  templateUrl: `./app/app.component.html`
+})
+export class AppComponent implements OnInit {
+  
+  pokemons: Pokemon[] = null;
+  
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+  
+  selectPokemon(pokemon: Pokemon) {
+    console.log('Vous avez selectionné ' + pokemon.name);
+  }
+}
