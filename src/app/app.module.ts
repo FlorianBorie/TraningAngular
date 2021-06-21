fa1489eede2b85ce057d30082297405be1e54faa
Florian Borie <florian.borie@ynov.com>
2021-06-21 10:43:51 +0200

    add module and organisation in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';  
+import { PokemonsModule } from './pokemons/pokemon.module';
   
 import { AppComponent }  from './app.component';
-import { ListPokemonComponent } from './list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found.component';
-
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
   
 @NgModule({
   imports:      [ 
-    BrowserModule, 
-    AppRoutingModule 
+    BrowserModule,
+    PokemonsModule,
+    AppRoutingModule
   ],
   declarations: [ 
-    AppComponent, 
-    BorderCardDirective, 
-    PokemonTypeColorPipe, 
-    ListPokemonComponent, 
-    DetailPokemonComponent,
+    AppComponent,
     PageNotFoundComponent
   ],
   bootstrap:    [ AppComponent ]
