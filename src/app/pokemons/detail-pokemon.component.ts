3c6cf22cb209933617676e16fb68dfa0d215b73d
Florian Borie <florian.borie@ynov.com>
2021-06-21 12:08:47 +0200

    add fomulaire for update pokemon

--- src/app/pokemons/detail-pokemon.component.ts
+++ src/app/pokemons/detail-pokemon.component.ts
@@ -22,5 +22,10 @@ export class DetailPokemonComponent implements OnInit {
     goBack(): void {
         this.router.navigate(['/pokemons']);
     }
+
+    goEdit(pokemon: Pokemon): void {
+        let link = ['pokemon/edit', pokemon.id]
+        this.router.navigate(link);
+    }
   
 }
